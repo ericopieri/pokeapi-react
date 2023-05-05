879af212a28c1747f5dab71495fd35b8a680f292
Erico Pieri <erico.pieri@gmail.com>
2023-05-05 17:52:31 -0300

    tela de pokemons iniciada + componente para titulo

---
+++ src/pages/Pokemons.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+
+import TitlePage from "../components/TitlePage";
+
+function Pokemons() {
+    return (
+        <div>
+            <TitlePage text="Lista de Pokemons" customClass="" />
+        </div>
+    );
+}
+
+export default Pokemons;
